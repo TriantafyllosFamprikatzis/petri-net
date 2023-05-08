bdd7f5379de94d471acf0f91d4bd55b42a7fe66e
Triantafyllos Famprikatzis <40291223+TriantafyllosFamprikatzis@users.noreply.github.com>
2023-05-08 17:06:16 +0300

    Fix token naming

--- hle58-01/petri_models/water/water_model.py
+++ hle58-01/petri_models/water/water_model.py
@@ -15,8 +15,8 @@ def water_transitions(
         insert_coin_button, select_product_button,
         messages_text, coin_small_10, coin_small_20,
         coin_small_50, coin_one, coin_two, token_TK_P1,
-        token_TK_P3, token_TK_P4, token_TK_P5, token_TK_P6,
-        token_TK_P7, token_TK_P8, token_TK_P10,
+        token_TK_P2, token_TK_P3, token_TK_P4, token_TK_P5,
+        token_TK_P6, token_TK_P7,
 ):
     global canRunTransition0
     # Define the Petri net transitions for water
@@ -75,26 +75,20 @@ def water_transitions(
             return False
 
     def transition9():
-        if currentValue == 50:
+        if currentValue >= productsList["water"]["value"]:
             return True
         else:
             return False
 
     def transition10():
-        if currentValue == 50:
+        if currentValue >= productsList["water"]["value"]:
             return True
         else:
             return False
 
-    def transition11():
-        if currentValue >= productsList["water"]["value"]:
-            return True
-        else:
-            return False 
-
     if transition0() and canRunTransition0:
         canvas.itemconfigure(token_TK_P1, state="hidden")
-        canvas.itemconfigure(token_TK_P3, state="normal")
+        canvas.itemconfigure(token_TK_P2, state="normal")
 
         water_radio_button.config(state="disabled")
         icetea_radio_button.config(state="disabled")
@@ -112,46 +106,41 @@ def water_transitions(
         messageHandler(messages_text, "Το προϊόν προστέθηκε!")
         canRunTransition0 = False
 
-    if transition1() or transition11():
-        canvas.itemconfigure(token_TK_P10, state="normal")
-        tokenHandler(canvas, "token_TK_P10", tokens)
+    if transition1() or transition9() or transition10():
+        canvas.itemconfigure(token_TK_P7, state="normal")
+        tokenHandler(canvas, "token_TK_P7", tokens)
 
         # buy_product_button.config(state="active")
         # cancel_product_button.config(state="active")
         changeAmount = calculateChangeHandler(drink_var, productsList, currentValue)
         insertedAmount = numberConverterHandler(coin_var.get())
         messageHandler(messages_text, f"Εισαγωγή νομίσματος: €{insertedAmount}, το ποσό συμπληρώθηκε, έδωσε ρέστα €{changeAmount}")
+        messageHandler(messages_text, "Το προϊόν έχει κατανεμηθεί!")
 
     if transition2():
-        canvas.itemconfigure(token_TK_P4, state="normal")
-        tokenHandler(canvas,"token_TK_P4", tokens)
+        canvas.itemconfigure(token_TK_P3, state="normal")
+        tokenHandler(canvas,"token_TK_P3", tokens)
         remainingAmount = remainingValueHandler(drink_var, productsList, currentValue)
         insertedAmount = numberConverterHandler(coin_var.get())
         messageHandler(messages_text, f"Εισαγωγή νομίσματος: €{insertedAmount}, υπολείπονται ακόμη €{remainingAmount}")
     
     if transition3() or transition4():
+        canvas.itemconfigure(token_TK_P4, state="normal")
+        tokenHandler(canvas,"token_TK_P4", tokens)
+        remainingAmount = remainingValueHandler(drink_var, productsList, currentValue)
+        insertedAmount = numberConverterHandler(coin_var.get())
+        messageHandler(messages_text, f"Εισαγωγή νομίσματος: €{insertedAmount}, υπολείπονται ακόμη €{remainingAmount}")
+
+    if transition5() or transition6(): 
         canvas.itemconfigure(token_TK_P5, state="normal")
         tokenHandler(canvas,"token_TK_P5", tokens)
         remainingAmount = remainingValueHandler(drink_var, productsList, currentValue)
         insertedAmount = numberConverterHandler(coin_var.get())
         messageHandler(messages_text, f"Εισαγωγή νομίσματος: €{insertedAmount}, υπολείπονται ακόμη €{remainingAmount}")
 
-    if transition5() or transition6(): 
+    if transition7() or transition8():
         canvas.itemconfigure(token_TK_P6, state="normal")
         tokenHandler(canvas,"token_TK_P6", tokens)
         remainingAmount = remainingValueHandler(drink_var, productsList, currentValue)
         insertedAmount = numberConverterHandler(coin_var.get())
-        messageHandler(messages_text, f"Εισαγωγή νομίσματος: €{insertedAmount}, υπολείπονται ακόμη €{remainingAmount}")
-
-    if transition7() or transition8():
-        canvas.itemconfigure(token_TK_P7, state="normal")
-        tokenHandler(canvas,"token_TK_P7", tokens)
-        remainingAmount = remainingValueHandler(drink_var, productsList, currentValue)
-        insertedAmount = numberConverterHandler(coin_var.get())
         messageHandler(messages_text,f"Εισαγωγή νομίσματος: €{insertedAmount}, υπολείπονται ακόμη €{remainingAmount}")
-
-    if transition9() or transition10():
-        canvas.itemconfigure(token_TK_P8, state="normal")
-        tokenHandler(canvas,"token_TK_P8", tokens)
-        remainingAmount = remainingValueHandler(drink_var, productsList, currentValue)
-        insertedAmount = numberConverterHandler(coin_var.get())
